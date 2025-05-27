5ad264e3f2e9264e794aaeb94715ed9a7f45ba65
wahabdif <wahab.diffallah@outlook.com>
2025-05-27 01:26:51 -0400

    Create practicalInfo.ts

---
+++ client/src/lib/data/practicalInfo.ts
@@ -0,0 +1,26 @@
+import { IconType } from "react-icons";
+import { FiClock, FiMapPin, FiPhone } from "react-icons/fi";
+
+export interface PracticalInfoItem {
+  icon: IconType;
+  titleKey: string;
+  detailKey: string;
+}
+
+export const practicalInfo: PracticalInfoItem[] = [
+  {
+    icon: FiClock,
+    titleKey: "info.openingHours.title",
+    detailKey: "info.openingHours.detail",
+  },
+  {
+    icon: FiMapPin,
+    titleKey: "info.address.title",
+    detailKey: "info.address.detail",
+  },
+  {
+    icon: FiPhone,
+    titleKey: "info.contact.title",
+    detailKey: "info.contact.detail",
+  },
+];
