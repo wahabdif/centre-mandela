06bdbbb64ca494d0c098ddf0720464ed338adddd
wahabdif <wahab.diffallah@outlook.com>
2025-05-27 15:23:24 -0400

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,24 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: ".", // client est la racine du projet client (depuis client/)
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@shared": path.resolve(__dirname, "../shared"), // accès code partagé
-    },
-  },
-  build: {
-    outDir: "../dist/client", // build React sort dans dist/client au niveau racine
-    emptyOutDir: true,
-    sourcemap: true, // utile en dev ou debug
-    // base: "/", // optionnel, surtout si tu déploies sous un chemin custom
-  },
+  
   server: {
     port: 3000,
-    strictPort: true,
+    open: true,
   },
-});
+
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+
+  preview: {
+    allowedHosts: ['centre-mandela-qscm.onrender.com'],
+  },
+
+  // Ajoutez ici d’autres configurations si nécessaire
+})
