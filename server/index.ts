d24d38fce265bd4651b690552941fb4f3c46e898
wahabdif <wahab.diffallah@outlook.com>
2025-05-28 18:52:20 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -16,7 +16,7 @@ app.use('/api', contactRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   const root = path.resolve(__dirname, '../../client/dist');
-  console.log('Serving static files from:', root); // Pour debug
+  console.log('Serving static files from:', root);
   app.use(express.static(root));
   app.get('*', (_, res) => {
     res.sendFile(path.join(root, 'index.html'));
