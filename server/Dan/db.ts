c087801ece9a75e23f8085d942aff3c5777c2f07
wahabdif <wahab.diffallah@outlook.com>
2025-05-28 14:09:17 -0400

    Create db.ts

---
+++ server/Dan/db.ts
@@ -0,0 +1,12 @@
+// server/db.ts
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  host: 'localhost',
+  port: 5432,
+  user: 'ton_user',
+  password: 'ton_password',
+  database: 'ta_db',
+});
+
+export default pool;
