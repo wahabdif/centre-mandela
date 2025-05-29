b9c20ebb97fb0de05513b60102d944a0ea2e0a8a
wahabdif <wahab.diffallah@outlook.com>
2025-05-28 22:37:44 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,20 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Toaster } from "@/components/ui/toaster";
 
-createRoot(document.getElementById("root")!).render(
-  <>
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root container introuvable");
+}
+
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
     <App />
     <Toaster />
-  </>
+  </React.StrictMode>
 );
