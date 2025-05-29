fb2a80988571b35b425e5b79aee722f4414f378f
wahabdif <wahab.diffallah@outlook.com>
2025-05-29 16:06:17 -0400

    Create babel.config.js

---
+++ client/babel.config.js
@@ -0,0 +1,17 @@
+// client/babel.config.js
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }]
+  ],
+  plugins: [
+    ['i18next-extract', {
+      outputPath: 'src/locales/{{locale}}/{{ns}}.json',
+      locales: ['fr', 'en', 'ar'],
+      keyAsDefaultValue: true,
+      useI18nextDefaultValue: ['fr'],
+      discardOldKeys: true,
+      failOnMissing: false
+    }]
+  ]
+};
