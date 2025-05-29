cb350ef7c0fdc776ff7e65c40c2c7773890984c0
wahabdif <wahab.diffallah@outlook.com>
2025-05-29 04:39:24 -0400

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 // Paths absolus
 const outDirPath = path.resolve(__dirname, '../server/public');
-const postcssPath = path.resolve(__dirname, './postcss.config.ts');
+const postcssPath = path.resolve(__dirname, './postcss.config.js');
 
 export default defineConfig({
   root: __dirname,
