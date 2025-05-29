7155bc5c377b0963bfac84f27ea196899816a97a
wahabdif <wahab.diffallah@outlook.com>
2025-05-29 14:52:18 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,9 @@ import App from "./App";
 import "./index.css";
 import { Toaster } from "@/components/ui/toaster";
 
+// ⬇️ Import obligatoire pour initialiser la traduction
+import "@/lib/i18n";
+
 const container = document.getElementById("root");
 
 if (!container) {
