7522a9f1168837f5c03c72f5470433a51e1601f0
wahabdif <wahab.diffallah@outlook.com>
2025-06-02 10:31:19 -0400

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,8 @@
+// .eslintrc.js
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    'react',
-    'react-hooks',
-    'prettier',
-  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -15,29 +11,20 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   settings: {
-    react: {
-      version: 'detect',
-    },
+    react: { version: 'detect' },
   },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
+    ecmaFeatures: { jsx: true },
   },
-  ignorePatterns: ['node_modules/', 'dist/', 'build/'],
+  ignorePatterns: ['node_modules/', 'dist/', 'build/', 'client/dist'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-expressions': 'off',
     '@typescript-eslint/no-floating-promises': 'warn',
     '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
     'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-shadow': 'off',
-    'no-empty': ['error', { allowEmptyCatch: true }],
-    'curly': ['error', 'all'],
-    'prefer-object-has-own': 'error',
     'react/react-in-jsx-scope': 'off',
     'react/no-unescaped-entities': 'off',
     'react/jsx-no-bind': ['warn', { ignoreRefs: true, allowArrowFunctions: false }],
