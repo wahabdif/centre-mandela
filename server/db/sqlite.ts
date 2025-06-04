6530c298fbc325fee1a010dc321e353949a02b0b
wahabdif <wahab.diffallah@outlook.com>
2025-06-04 00:58:36 -0400

    Update sqlite.ts

--- server/db/sqlite.ts
+++ server/db/sqlite.ts
@@ -1,9 +1,9 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { drizzle } from 'drizzle-orm/sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from '../../shared/schema';
 
-// Ouvre ou crée la base SQLite
-const sqlite = new Database('data.sqlite');
+// Initialise SQLite (le fichier sera créé s'il n'existe pas)
+const sqlite = new Database('sqlite.db');
 
 // Initialise Drizzle avec le schéma
 export const db = drizzle(sqlite, { schema });
