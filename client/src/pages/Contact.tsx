8cb39481495b158042d2f722cb952ade8dfd872a
wahabdif <wahab.diffallah@outlook.com>
2025-06-05 18:53:32 -0400

    Update Contact.tsx

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -20,7 +20,7 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import {
-  LoaderCircle,
+  Loader,
   MapPin,
   Phone,
   Mail,
