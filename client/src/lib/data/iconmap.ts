971783c2495ef79fbf1ef3210ca8185ae75814f4
wahabdif <wahab.diffallah@outlook.com>
2025-06-05 19:05:44 -0400

    Create iconmap.ts

---
+++ client/src/lib/data/iconmap.ts
@@ -0,0 +1,27 @@
+import {
+  Brain,
+  Heart,
+  User,
+  Calendar,
+  Phone,
+  Mail,
+  Clock,
+  MapPin,
+  Facebook,
+  Instagram,
+  Linkedin,
+} from "lucide-react";
+
+export const iconMap = {
+  brain: Brain,
+  heart: Heart,
+  user: User,
+  calendar: Calendar,
+  phone: Phone,
+  mail: Mail,
+  clock: Clock,
+  mappin: MapPin,
+  facebook: Facebook,
+  instagram: Instagram,
+  linkedin: Linkedin,
+};
