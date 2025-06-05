52dcd811205cdc5e54609fc24dac59b093feb519
wahabdif <wahab.diffallah@outlook.com>
2025-06-05 19:39:20 -0400

    Update use-toast.ts

--- client/src/hooks/use-toast.ts
+++ client/src/hooks/use-toast.ts
@@ -185,7 +185,10 @@ function useToast() {
   return {
     ...state,
     toast,
-    dismiss: (toastId?: string) => dispatch({ type: 'DISMISS_TOAST', toastId }),
+    dismiss: (toastId?: string) =>
+      toastId
+        ? dispatch({ type: 'DISMISS_TOAST', toastId })
+        : dispatch({ type: 'DISMISS_TOAST' }),
   };
 }
 
